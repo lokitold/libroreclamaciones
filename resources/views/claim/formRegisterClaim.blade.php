dcb7aebfe0b84d967817baa9e91ad6c4c9b7fc0d
victor Rojas <victor.rojas@ec.pe>
2016-06-02 11:18:57 -0500

    cambi+

--- resources/views/claim/formRegisterClaim.blade.php
+++ resources/views/claim/formRegisterClaim.blade.php
@@ -9,44 +9,73 @@
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/reclamo') }}">
                             {{ csrf_field() }}
+                            <div class="panel panel-default">
+                                <div class="panel-heading">Identificación del consumidor reclamante</div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <label for="name" class="col-md-1 control-label"></label>
+                                        <div class="col-md-8">
+                                            <label><input type="radio" name="radio-tipo" value="persona"> Persona</label>
+                                            <label><input type="radio" name="radio-tipo" value="empresa"> Empresa</label>
+                                        </div>
+                                    </div>
 
-                            <div>
-                                <label><input type="radio" name="radio-tipo" value="persona"> Persona</label>
-                                <label><input type="radio" name="radio-tipo" value="empresa"> Empresa</label>
-                            </div>
+                                    <div class="persona box">
+                                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                            <label for="name" class="col-md-4 control-label">Nombre</label>
 
-                            <div class="persona box">
-                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                    <label for="name" class="col-md-4 control-label">Nombre</label>
-
-                                    <div class="col-md-6">
-                                        <input id="name" type="text" class="form-control" name="name"
-                                               value="{{ old('name') }}">
-                                        @if ($errors->has('name'))
-                                            <span class="help-block">
+                                            <div class="col-md-4">
+                                                <input id="name" type="text" class="form-control" name="name"
+                                                       value="{{ old('name') }}">
+                                                @if ($errors->has('name'))
+                                                    <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                        @endif
-                                    </div>
-                                </div>
+                                                @endif
+                                            </div>
+                                        </div>
 
-                                <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
-                                    <label for="lastname" class="col-md-4 control-label">Apellido paterno</label>
+                                        <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
+                                            <label for="lastname" class="col-md-4 control-label">Apellido paterno</label>
 
-                                    <div class="col-md-6">
-                                        <input id="lastname" type="text" class="form-control" name="lastname"
-                                               value="{{ old('lastname') }}">
+                                            <div class="col-md-4">
+                                                <input id="lastname" type="text" class="form-control" name="lastname"
+                                                       value="{{ old('lastname') }}">
 
-                                        @if ($errors->has('lastname'))
-                                            <span class="help-block">
+                                                @if ($errors->has('lastname'))
+                                                    <span class="help-block">
                                         <strong>{{ $errors->first('lastname') }}</strong>
                                     </span>
-                                        @endif
+                                                @endif
+                                            </div>
+                                        </div>
                                     </div>
+                                    <div class="empresa box">
+                                        <div class="form-group{{ $errors->has('business_name') ? ' has-error' : '' }}">
+                                            <label for="business_name" class="col-md-4 control-label">Razon social</label>
+
+                                            <div class="col-md-4">
+                                                <input id="business_name" type="text" class="form-control" name="business_name"
+                                                       value="{{ old('business_name') }}">
+                                                @if ($errors->has('business_name'))
+                                                    <span class="help-block">
+                                        <strong>{{ $errors->first('business_name') }}</strong>
+                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
-                            <div class="empresa box">
-                                Empresa
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Datos de Reclamación</h3>
+                                </div>
+                                <div class="panel-body">
+                                    Panel content
+                                </div>
                             </div>
 
                             <div class="form-group">
