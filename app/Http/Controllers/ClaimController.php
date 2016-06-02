379584f5d9f0c35485ea4ba8150e9404156fbba9
victor-pc <vico.16c@gmail.com>
2016-06-01 23:32:37 -0500

    cambio

--- app/Http/Controllers/ClaimController.php
+++ app/Http/Controllers/ClaimController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Product;
 
 class ClaimController extends Controller
 {
@@ -12,11 +13,16 @@ class ClaimController extends Controller
 
     public function index($productCode = null)
     {
-        if(empty($productCode)):
-            $productCode = $this->productDefault;
+
+        $product = Product::where('codigo', $productCode)
+            ->first();
+
+        if(empty($product)):
+            $product = Product::where('codigo', $this->productDefault)
+                ->first();
         endif;
 
         return view('claim.formRegisterClaim')
-            ->with('productCode',$productCode);
+            ->with('productCode',$product->codigo);
     }
 }
