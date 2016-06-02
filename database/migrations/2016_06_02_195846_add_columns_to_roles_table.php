470a5bfdb5a00533e7237ddcb2b87add983e93a4
victor Rojas <victor.rojas@ec.pe>
2016-06-02 15:09:37 -0500

    cmbio

---
+++ database/migrations/2016_06_02_195846_add_columns_to_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+            $table->string('name');
+            $table->integer('status')->length(1)->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+            $table->dropColumn('name', 'status');
+        });
+    }
+}
