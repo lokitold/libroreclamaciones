72069cd1424248eb3de1c4e9c8a0bda1e0a6b0e4
victor Rojas <victor.rojas@ec.pe>
2016-06-03 11:21:59 -0500

    vcmbni

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         \InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
         \InfyOm\CoreTemplates\CoreTemplatesServiceProvider::class,
+        InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class,
 
     ],
 
